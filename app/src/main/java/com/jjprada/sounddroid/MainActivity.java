5ade2c52d5134bc77852afc0e3b804dd9c6fdae3
jjprada <jjprada@gmail.com>
2015-03-26 19:11:22 +0100

    Retrofit Requests

--- app/src/main/java/com/jjprada/sounddroid/MainActivity.java
+++ app/src/main/java/com/jjprada/sounddroid/MainActivity.java
@@ -15,6 +15,11 @@ import org.json.JSONObject;
 import java.lang.reflect.Type;
 import java.util.List;
 
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -25,17 +30,22 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Gson gson = new Gson();
-        Type type = new TypeToken<List<Track>>(){}.getType();
-        List<Track> tracks = gson.fromJson(trackJSON(), type);
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setLogLevel(RestAdapter.LogLevel.FULL)
+                .setEndpoint("http://api.soundcloud.com")
+                .build();
+        SoundCloudService service = restAdapter.create(SoundCloudService.class);
+        service.searchSongs("dark horse", new Callback<List<Track>>() {
+            @Override
+            public void success(List<Track> tracks, Response response) {
+                Log.d(TAG, "Track title is "+tracks.get(0).getTitle());
+            }
 
-        Log.d(TAG, "ID is: "+tracks.get(0).getID());
-        Log.d(TAG, "Title is: "+tracks.get(0).getTitle());
-        Log.d(TAG, "StreamURL is: "+tracks.get(0).getStreamURL());
-    }
+            @Override
+            public void failure(RetrofitError error) {
 
-    private String trackJSON(){
-        return "{\"kind\":\"track\",\"id\":13158665,\"created_at\":\"2011/04/06 15:37:43 +0000\",\"user_id\":3699101,\"duration\":18109,\"commentable\":true,\"state\":\"finished\",\"original_content_size\":201483,\"last_modified\":\"2013/02/18 19:18:11 +0000\",\"sharing\":\"public\",\"tag_list\":\"soundcloud:source=iphone-record\",\"permalink\":\"munching-at-tiannas-house\",\"streamable\":true,\"embeddable_by\":\"all\",\"downloadable\":false,\"purchase_url\":null,\"label_id\":null,\"purchase_title\":null,\"genre\":null,\"title\":\"Munching at Tiannas house\",\"description\":null,\"label_name\":null,\"release\":null,\"track_type\":\"recording\",\"key_signature\":null,\"isrc\":null,\"video_url\":null,\"bpm\":null,\"release_year\":null,\"release_month\":null,\"release_day\":null,\"original_format\":\"m4a\",\"license\":\"all-rights-reserved\",\"uri\":\"https://api.soundcloud.com/tracks/13158665\",\"user\":{\"id\":3699101,\"kind\":\"user\",\"permalink\":\"alex-stevenson\",\"username\":\"Alex Stevenson\",\"last_modified\":\"2011/06/13 23:58:44 +0000\",\"uri\":\"https://api.soundcloud.com/users/3699101\",\"permalink_url\":\"http://soundcloud.com/alex-stevenson\",\"avatar_url\":\"https://i1.sndcdn.com/avatars-000004193858-jnf2pd-large.jpg\"},\"created_with\":{\"id\":124,\"kind\":\"app\",\"name\":\"SoundCloud iOS\",\"uri\":\"https://api.soundcloud.com/apps/124\",\"permalink_url\":\"http://soundcloud.com/apps/iphone\",\"external_url\":\"http://itunes.com/app/soundcloud\"},\"permalink_url\":\"http://soundcloud.com/alex-stevenson/munching-at-tiannas-house\",\"artwork_url\":null,\"waveform_url\":\"https://w1.sndcdn.com/fxguEjG4ax6B_m.png\",\"stream_url\":\"https://api.soundcloud.com/tracks/13158665/stream\",\"playback_count\":5655,\"download_count\":134,\"favoritings_count\":2,\"comment_count\":3,\"attachments_uri\":\"https://api.soundcloud.com/tracks/13158665/attachments\",\"policy\":\"ALLOW\"}";
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/jjprada/sounddroid/SoundCloudService.java
@@ -0,0 +1,18 @@
+package com.jjprada.sounddroid;
+
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Query;
+
+/**
+ * Created by Dr4ckO on 26/03/2015.
+ */
+public interface SoundCloudService {
+
+    static final String CLIENT_ID = "ee2445801ff5b449eac7c77a6c65c3e4";
+
+    @GET("/tracks?client_id="+CLIENT_ID)
+    public void searchSongs(@Query("q") String query, Callback<List<Track>> cb);
+}

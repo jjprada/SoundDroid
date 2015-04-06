d14e727b4454b5ce649405415f22846d228e349a
jjprada <jjprada@gmail.com>
2015-04-06 19:14:27 +0200

    Adding Artwork

--- app/src/main/java/com/jjprada/sounddroid/TracksAdapter.java
+++ app/src/main/java/com/jjprada/sounddroid/TracksAdapter.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.jjprada.sounddroid.com.jjprada.soundroid.soundcloud.Track;
@@ -17,10 +18,12 @@ public class TracksAdapter extends RecyclerView.Adapter<TracksAdapter.ViewHolder
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         private final TextView titleTextView;
+        private final ImageView thumbImageView;
 
         ViewHolder(View v) {                // Constructor de "ViewHolder"
             super(v);
             titleTextView = (TextView)v.findViewById(R.id.track_title);         // Usamos esto solo una vez, porque consume mucho tiempo
+            thumbImageView = (ImageView)v.findViewById(R.id.track_thumbnail);
         }
     }
 
@@ -46,6 +49,7 @@ public class TracksAdapter extends RecyclerView.Adapter<TracksAdapter.ViewHolder
     public void onBindViewHolder(ViewHolder holder, int position) {
         Track track = mTracks.get(position);
         holder.titleTextView.setText(track.getTitle());
+        holder.thumbImageView.setImageResource();
     }
 
 
--- app/src/main/java/com/jjprada/sounddroid/com/jjprada/soundroid/soundcloud/Track.java
+++ app/src/main/java/com/jjprada/sounddroid/com/jjprada/soundroid/soundcloud/Track.java
@@ -20,28 +20,30 @@ public class Track {
     @SerializedName("stream_url")
     private String mStreamURL;
 
+    @SerializedName("artwork_url")
+    private String mArtworkURL;
+
     public int getID() {
         return mID;
-    }
-
-    public void setID(int ID) {
-        mID = ID;
     }
 
     public String getTitle() {
         return mTitle;
     }
 
-    public void setTitle(String title) {
-        mTitle = title;
-    }
-
     public String getStreamURL() {
         return mStreamURL;
     }
 
-    public void setStreamURL(String streamURL) {
-        mStreamURL = streamURL;
+    public String getArtworkURL() {
+        return mArtworkURL;
     }
 
+    public String getAvatarURL(){
+        String avatarURL = mArtworkURL;
+        if (avatarURL != null){         // Chequeo de segurida para que no haya un "crash" si "mArtWorkURL" es null
+            avatarURL = mArtworkURL.replace("large", "tiny");
+        }
+        return avatarURL;
+    }
 }
